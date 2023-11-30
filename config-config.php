35ad13864557d480684706380bbc6b37bc2c9e2e
JuliethMahecha <151239375+JuliethMahecha@users.noreply.github.com>
2023-11-30 17:01:29 -0500

    Create config-config.php

---
+++ config-config.php
@@ -0,0 +1,15 @@
+<?php
+
+    $errorResponse = array(
+        0 => "Operación satisfactoria",
+        1 => "No conecta al servidor",
+        2 => "Registro ya Existe",
+        3 => "Acceso a API con metodo incorrecto",
+        4 => "No hay registros",
+        5 => "Datos incompletos",
+        10=> "Error de Operación"
+    );
+
+    
+
+?>
